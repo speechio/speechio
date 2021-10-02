faac576bcbe87af71147618593dcf1c9279d97e2
dophist <jerry.ejwt@qq.com>
2021-10-02 13:49:21 +0800

    reorg gigaspeech subset dir, one subdir per long-audio

--- utils/gigaspeech_extract_subset.py
+++ utils/gigaspeech_extract_subset.py
@@ -15,19 +15,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Save the audio segments into flac files.')
     parser.add_argument('--subset', choices = ['XS', 'S', 'M', 'L', 'XL', 'DEV', 'TEST'], default='XS', help='The subset name')
     parser.add_argument('gigaspeech_dataset_dir', help='The corpus directory')
-    parser.add_argument('output', help='The output directory')
+    parser.add_argument('dst_dir', help='The dst_dir directory')
     args = parser.parse_args()
 
-    audio_dir = os.path.join(args.output, 'audio')
-    os.makedirs(audio_dir, exist_ok = True)
+    os.makedirs(args.dst_dir, exist_ok = True)
 
     gigaspeech = GigaSpeech(args.gigaspeech_dataset_dir)
     subset = '{' + args.subset + '}'
-    with open(os.path.join(args.output, 'metadata.tsv'), 'w+', encoding='utf8') as fo:
+    with open(os.path.join(args.dst_dir, 'metadata.tsv'), 'w+', encoding='utf8') as fo:
         csv_header_fields = ['ID', 'AUDIO', 'DURATION', 'TEXT']
         csv_writer = csv.DictWriter(fo, delimiter='\t', fieldnames=csv_header_fields, lineterminator='\n')
         csv_writer.writeheader()
         for audio in gigaspeech.audios(subset):
+            aid = audio['aid']
             audio_path = os.path.join(args.gigaspeech_dataset_dir, audio["path"])
 
             audio_info = torchaudio.info(audio_path)
@@ -40,12 +40,13 @@ if __name__ == '__main__':
             long_waveform, _ = torchaudio.load(audio_path)
             long_waveform = torchaudio.transforms.Resample(opus_sample_rate, sample_rate)(long_waveform)
 
-            #print(audio_info)
-            for seg in audio['segments']:
-                if subset not in seg['subsets']:
+            for segment in audio['segments']:
+                sid = segment['sid']
+
+                if subset not in segment['subsets']:
                     continue
 
-                text = seg['text_tn']
+                text = segment['text_tn']
                 for punctuation in gigaspeech_punctuations:
                     text = text.replace(punctuation, '').strip()
                     text = ' '.join(text.split())
@@ -53,16 +54,17 @@ if __name__ == '__main__':
                 if text in gigaspeech_garbage_utterance_tags:
                     continue
 
-                begin = seg['begin_time'] 
-                duration = seg['end_time'] - seg['begin_time']
+                begin = segment['begin_time'] 
+                duration = segment['end_time'] - segment['begin_time']
                 frame_offset = int(begin    * sample_rate)
                 num_frames   = int(duration * sample_rate)
 
-                waveform = long_waveform[0][frame_offset : frame_offset + num_frames] # channel = mono 
+                waveform = long_waveform[0][frame_offset : frame_offset + num_frames] # mono
 
-                os.makedirs(os.path.join(audio_dir, audio['aid']), exist_ok = True)
+                segment_path = os.path.join('audio', aid, f'{sid}.wav')
+                os.makedirs(os.path.join(args.dst_dir, os.path.dirname(segment_path)), exist_ok = True)
                 torchaudio.save(
-                    os.path.join(audio_dir, audio['aid'], f'{seg["sid"]}.wav'), 
+                    os.path.join(args.dst_dir, segment_path),
                     waveform.unsqueeze(0), 
                     sample_rate = sample_rate,
                     format = 'wav',
@@ -70,7 +72,6 @@ if __name__ == '__main__':
                     bits_per_sample = 16,
                 )
 
-                utt = {'ID': seg['sid'], 'AUDIO': os.path.join('audio', audio['aid'], f'{seg["sid"]}.wav'), 'DURATION': f'{duration:.3f}', 'TEXT': text }
+                utt = {'ID': segment['sid'], 'AUDIO': segment_path, 'DURATION': f'{duration:.3f}', 'TEXT': text }
                 csv_writer.writerow(utt)
-            break
 
