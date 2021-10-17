7b42e496f000a3eed3681827bea722d6ea713c51
dophist <jerry.jiayu.du@gmail.com>
2021-10-17 12:26:22 +0800

    reorg

--- runtime/sito.cc
+++ runtime/sito.cc
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
 
-#include "check.h"
-#include "type.h"
-#include "absl/container/flat_hash_map.h"
+#include "sito/check.h"
+#include "sito/type.h"
+//#include "absl/container/flat_hash_map.h"
 
 int main() {
     using namespace sio;
     P_CHECK(true);
-    std::vector<std::string> v = {"foo","bar","baz"};
+    std::vector<str> v = {"foo","bar","baz"};
     str s = absl::StrJoin(v, "-");
     str_view sv = s;
     I_CHECK(true);
@@ -17,7 +17,7 @@ int main() {
     i32 i = 10;
     ref<i32*> p = &i;
     std::cout << p << "\n";
-    absl::flat_hash_map<int, str> m;
+    //absl::flat_hash_map<int, str> m;
     std::cout << &m << "\n";
 
     Q_CHECK(false);
---
+++ train.sh
@@ -0,0 +1,11 @@
+## Tokenizer
+# ops/train_tokenizer --config configs/train_tokenizer.yaml --input misc/text --model misc/tokenizer
+
+# ops/tokenizer_encode --input misc/text --model misc/tokenizer --output_format id --output misc/encoded1
+# cat misc/text | ops/tokenizer_encode --model misc/tokenizer > misc/encoded2
+
+# ops/tokenizer_decode --model misc/tokenizer --input misc/encoded1 --input_format id > misc/decoded1
+# cat misc/encoded2 | ops/tokenizer_decode --model misc/tokenizer > misc/decoded2
+
+## Train
+ops/train_sito --config config/train_sito.yaml
