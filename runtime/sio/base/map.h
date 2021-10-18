0b22c1116c73fa055fad59a7df788d5e0c1a882c
dophist <jerry.jiayu.du@gmail.com>
2021-10-18 16:58:33 +0800

    fix map & gmap definition

--- runtime/sio/base/map.h
+++ runtime/sio/base/map.h
@@ -9,21 +9,20 @@ namespace sio {
 template <
     class K,
     class V,
-    class Hash = absl::container_internal::hash_default_hash<K>,
-    class Eq = absl::container_internal::hash_default_eq<K>,
-    class Allocator = std::allocator<std::pair<const K, V>>
->
-using map = absl::flat_hash_map<K, V, Hash, Eq, Allocator>;
-
-
-template <
-    class K,
-    class V,
     class Hash = std::hash<K>,
     class Eq = std::equal_to<K>,
     class Allocator = std::allocator<std::pair<const K, V>>
 >
-using gmap = std::unordered_map<K, V, Hash, Eq, Allocator>;
+using map = std::unordered_map<K, V, Hash, Eq, Allocator>;
+
+template <
+    class K,
+    class V,
+    class Hash = absl::container_internal::hash_default_hash<K>,
+    class Eq = absl::container_internal::hash_default_eq<K>,
+    class Allocator = std::allocator<std::pair<const K, V>>
+>
+using gmap = absl::flat_hash_map<K, V, Hash, Eq, Allocator>;
 
 };
 
