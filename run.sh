77df413c6693dbafd81003c8e120c976db94932e
dophist <jerry.ejwt@qq.com>
2021-11-09 20:07:50 +0800

    init ctc-att joint model

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 ## Tokenizer
-# ops/train_tokenizer --config configs/train_tokenizer.yaml --input misc/text --model misc/tokenizer
+#ops/train_tokenizer --language ZH --config configs/train_tokenizer.yaml --input data/text/AISHELL-1_trn.txt --model model/tokenizer/AISHELL-1
+#ops/train_tokenizer --language EN --config configs/train_tokenizer.yaml --input data/text/GigaSpeech_S.txt --model model/tokenizer/GigaSpeech_S
 
 # ops/tokenizer_encode --input misc/text --model misc/tokenizer --output_format id --output misc/encoded1
 # cat misc/text | ops/tokenizer_encode --model misc/tokenizer > misc/encoded2
@@ -8,4 +9,4 @@
 # cat misc/encoded2 | ops/tokenizer_decode --model misc/tokenizer > misc/decoded2
 
 ## Train
-ops/train_sito --config configs/train_sito.yaml
+ops/train_sito --config configs/train_sito_en.yaml
