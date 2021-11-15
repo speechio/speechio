db36f351b88afa56c97f695791898f33c2f9c4ca
dophist <jerry.ejwt@qq.com>
2021-11-15 23:14:34 +0800

    add train/test subcommand to stt

--- run.sh
+++ run.sh
@@ -11,4 +11,5 @@
 ## Train
 dir=exp/tmp
 mkdir -p $dir
-ops/train_stt --config config/train_stt_zh.yaml $dir
+ops/stt train --config config/stt_zh.yaml $dir
+ops/stt test  --config config/stt_zh.yaml $dir
