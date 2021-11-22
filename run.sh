48231d494d1543f1a0362866e66ab0412560f327
dophist <jerry.ejwt@qq.com>
2021-11-22 21:03:40 +0800

    put top level recipe in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-## Tokenizer
+## MEMO
 #ops/tokenizer_train --config config/tokenizer_zh.yaml --input data/text/AISHELL-1_trn.txt --model model/tokenizer/AISHELL-1
 #ops/tokenizer_train --config config/tokenizer_en.yaml --input data/text/GigaSpeech_S.txt --model model/tokenizer/GigaSpeech_S
 
@@ -8,7 +8,12 @@
 # ops/tokenizer_decode --model misc/tokenizer --input misc/encoded1 --input_format id > misc/decoded1
 # cat misc/encoded2 | ops/tokenizer_decode --model misc/tokenizer > misc/decoded2
 
-## Train
+## RECIPE
+
+#ops/tokenizer_train --config config/tokenizer_zh.yaml --input data/text/AISHELL-1_trn.txt --model model/tokenizer/AISHELL-1
+
 dir=exp/ddp && mkdir -p $dir
-ops/stt_train --node_rank 0 --config config/train_stt_zh.yaml $dir
+#ops/stt_train --node_rank 0 --config config/train_stt_zh.yaml $dir
+ops/model_average --begin 161 --end 181 $dir/checkpoints $dir/final.ckpt
+
 #ops/stt_test --config config/stt_test_zh.yaml $dir
