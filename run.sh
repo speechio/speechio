6fbf9a6d4a59a097218c1bfc52c40cfbbb0d1e1d
dophist <jerry.ejwt@qq.com>
2021-11-22 21:45:29 +0800

    add recipe
    covering:
    1. tokenizer training
    2. stt model training
    3. test set decoding
    4. error rate evaluation

--- run.sh
+++ run.sh
@@ -10,10 +10,18 @@
 
 ## RECIPE
 
+## Train tokenizer
 #ops/tokenizer_train --config config/tokenizer_zh.yaml --input data/text/AISHELL-1_trn.txt --model model/tokenizer/AISHELL-1
 
+## Train stt model
 dir=exp/ddp && mkdir -p $dir
 #ops/stt_train --node_rank 0 --config config/train_stt_zh.yaml $dir
-ops/model_average --begin 161 --end 181 $dir/checkpoints $dir/final.ckpt
+#ops/model_average --begin 161 --end 181 $dir/checkpoints $dir/final.ckpt
 
-#ops/stt_test --config config/stt_test_zh.yaml $dir
+## Infererence
+#ops/stt_test --config config/stt_test_zh.yaml $dir 1> res.test
+
+## Error rate evaluation
+awk -F'\t' '{print $2, $3}' res.test > rec.txt
+ops/compute_error_rate --tokenizer char --ref ref.txt --hyp rec.txt RESULT
+
