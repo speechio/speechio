a287017825feee9f1a0b3a9b433adf30860fdd13
dophist <jerry.ejwt@qq.com>
2021-11-22 22:40:40 +0800

    clean recipe

--- run.sh
+++ run.sh
@@ -8,20 +8,20 @@
 # ops/tokenizer_decode --model misc/tokenizer --input misc/encoded1 --input_format id > misc/decoded1
 # cat misc/encoded2 | ops/tokenizer_decode --model misc/tokenizer > misc/decoded2
 
-## RECIPE
+#-------------------- RECIPE --------------------#
 
-## Train tokenizer
-#ops/tokenizer_train --config config/tokenizer_zh.yaml --input data/text/AISHELL-1_trn.txt --model model/tokenizer/AISHELL-1
+# Train tokenizer
+ops/tokenizer_train --config config/tokenizer_zh.yaml --input data/text/AISHELL-1_trn.txt --model model/tokenizer/AISHELL-1
 
-## Train stt model
-#dir=exp/ddp && mkdir -p $dir
-#ops/stt_train --node_rank 0 --config config/train_stt_zh.yaml $dir
-#ops/model_average --begin 161 --end 181 $dir/checkpoints $dir/final.ckpt
+# Train stt model
+dir=exp/ddp && mkdir -p $dir
+ops/stt_train --node_rank 0 --config config/train_stt_zh.yaml $dir 2> $dir/log.train
+ops/model_average --begin 161 --end 181 $dir/checkpoints $dir/final.ckpt
 
-## Inference on test set
-#ops/stt_test --config config/stt_test_zh.yaml $dir 1> $dir/res.test
+# Inference on test set
+ops/stt_test --config config/stt_test_zh.yaml $dir 1> $dir/res.test 2> $dir/log.test
 
-## Error rate evaluation
+# Error rate evaluation
 awk -F'\t' '{print $2, $3}' $dir/res.test > $dir/rec.txt
 ops/compute_error_rate --tokenizer char --ref $dir/ref.txt --hyp $dir/rec.txt $dir/RESULT
 
