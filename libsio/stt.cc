73690624e1a317c6691c6362f5a2df358137f76f
dophist <jerry.ejwt@qq.com>
2021-12-09 10:13:51 +0800

    seperate sample rate of model/audio with checking

--- libsio/stt.cc
+++ libsio/stt.cc
@@ -18,32 +18,35 @@ int main() {
     using namespace kaldi;
     using namespace sio;
 
+    float sample_rate = 16000;
     float chunk_secs = 0.2;
 
-    i32 num_err = 0;
+    int chunk_samples;
+    if (chunk_secs > 0) {
+        chunk_samples = std::max(1, int(sample_rate * chunk_secs));
+    } else {
+        chunk_samples = std::numeric_limits<int>::max();
+    }
+
     std::ifstream wav_scp("testdata/MINI/wav.scp");
     std::string line;
     while (getline(wav_scp, line)) {
-        std::vector<std::string> cols = absl::StrSplit(line, absl::ByAnyChar(" \t,:;"));
-        if (cols.size() != 2) continue;
+        std::vector<std::string> fields = absl::StrSplit(line, absl::ByAnyChar(" \t,:;"));
+        if (fields.size() != 2) continue;
 
-        std::string audio_key  = cols[0];
-        std::string audio_path = cols[1];
+        std::string& audio_key  = fields[0];
+        std::string& audio_path = fields[1];
         SIO_DEBUG << audio_key << " " << audio_path;
 
         WaveData wave_data;
         std::ifstream stream(audio_path, std::ifstream::binary);
         wave_data.Read(stream);
-        float sample_rate = wave_data.SampFreq();
-        SIO_CHECK(sample_rate == 16000, "sample rate is not 16k.");
         SubVector<BaseFloat> audio(wave_data.Data(), 0); // only use channel 0
 
-        int chunk_samples;
-        if (chunk_secs > 0) {
-            chunk_samples = std::max(1, int(sample_rate * chunk_secs));
-        } else {
-            chunk_samples = std::numeric_limits<int>::max();
-        }
+        SIO_CHECK(
+            wave_data.SampFreq() == sample_rate,
+            "inconsistent sample rate between audio/model."
+        );
 
         OnlineTimer decoding_timer(audio_key);
         //recognizer->StartSession(audio_key.c_str());
@@ -61,7 +64,7 @@ int main() {
             //if (opts.do_endpointing && recognizer->EndOfSentenceDetected()) {
             //    break;
             //}
-            SIO_DEBUG << audio_key << ": " << samples_done << " samples decoded.";
+            SIO_DEBUG << samples_done << " samples decoded.";
         }
         SIO_INFO << "Decoded " << samples_done << " samples for " << audio_key;
         //recognizer->StopSession();
