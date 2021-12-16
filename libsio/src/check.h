db43f4dfcc83927a60549ee5423765cd290b96fd
dophist <jerry.ejwt@qq.com>
2021-12-16 16:04:29 +0800

    reorder SIO_CHECK args

--- libsio/src/check.h
+++ libsio/src/check.h
@@ -5,16 +5,16 @@
 #include "sio/log.h"
 #include "sio/error.h"
 
-#define SIO_CHECK(expr, err_msg, err) do {                  \
+#define SIO_CHECK(expr, err, err_msg) do {                  \
   if SIO_UNLIKELY(!(expr)) {                                \
     SIO_FATAL << "{" << #expr << "} -> false. " << err_msg; \
     SIO_PANIC(err);                                         \
   }                                                         \
 } while(0)
 
-#define SIO_P_COND(cond) SIO_CHECK(cond, "", ::sio::Error::Precondition)
-#define SIO_Q_COND(cond) SIO_CHECK(cond, "", ::sio::Error::Postcondition)
-#define SIO_INVAR(cond)  SIO_CHECK(cond, "", ::sio::Error::Invariant)
+#define SIO_P_COND(cond) SIO_CHECK(cond, ::sio::Error::Precondition, "")
+#define SIO_Q_COND(cond) SIO_CHECK(cond, ::sio::Error::Postcondition, "")
+#define SIO_INVAR(cond)  SIO_CHECK(cond, ::sio::Error::Invariant, "")
 
 
 #endif
