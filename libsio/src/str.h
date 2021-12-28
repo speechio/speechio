24e6a09e5351beb0698681608adf138373676d12
dophist <jerry.ejwt@qq.com>
2021-12-28 20:51:29 +0800

    use complete headers of absl::strings lib

--- libsio/src/str.h
+++ libsio/src/str.h
@@ -3,12 +3,18 @@
 
 #include <string>
 
-#include <absl/strings/string_view.h>
-#include <absl/strings/strip.h>
-#include <absl/strings/str_join.h>
-#include <absl/strings/str_split.h>
+#include <absl/strings/ascii.h>
+#include <absl/strings/charconv.h>
+#include <absl/strings/escaping.h>
+#include <absl/strings/match.h>
+#include <absl/strings/numbers.h>
 #include <absl/strings/str_cat.h>
+#include <absl/strings/str_join.h>
 #include <absl/strings/str_replace.h>
+#include <absl/strings/str_split.h>
+#include <absl/strings/strip.h>
+#include <absl/strings/substitute.h>
+#include <absl/strings/string_view.h>
 #include <absl/strings/str_format.h>
 
 namespace sio {
