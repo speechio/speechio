ad40686442b678ef25904f93633f6e64a0549764
dophist <jerry.ejwt@qq.com>
2021-12-30 17:00:50 +0800

    cosmetic renaming

--- libsio/src/feature.h
+++ libsio/src/feature.h
@@ -8,12 +8,12 @@
 #include "sio/mean_var_norm.h"
 namespace sio {
 struct FeatureExtractorConfig {
-  kaldi::OnlineNnet2FeaturePipelineConfig kaldi;
+  kaldi::OnlineNnet2FeaturePipelineConfig kaldi_feat_config;
   std::string mean_var_norm_file;
 
   Error Register(StructLoader* loader, const std::string module = "") {
-    loader->AddEntry(module, ".type", &kaldi.feature_type);
-    loader->AddEntry(module, ".fbank_config", &kaldi.fbank_config);
+    loader->AddEntry(module, ".type", &kaldi_feat_config.feature_type);
+    loader->AddEntry(module, ".fbank_config", &kaldi_feat_config.fbank_config);
     loader->AddEntry(module, ".mean_var_norm_file", &mean_var_norm_file);
     return Error::OK;
   }
@@ -22,57 +22,59 @@ struct FeatureExtractorConfig {
 class FeatureExtractor {
  public:
   FeatureExtractor(const FeatureExtractorConfig& config) :
-    info_(config.kaldi)
+    kaldi_feat_info_(config.kaldi_feat_config)
   { 
-    kaldi_feature_pipeline_ = new kaldi::OnlineNnet2FeaturePipeline(info_);
+    kaldi_feat_pipe_ = new kaldi::OnlineNnet2FeaturePipeline(kaldi_feat_info_);
 
     if (config.mean_var_norm_file != "") {
       mean_var_norm_ = new MeanVarNorm(config.mean_var_norm_file);
+    } else {
+      mean_var_norm_ = nullptr;
     }
   }
 
   ~FeatureExtractor() {
-    delete kaldi_feature_pipeline_;
+    delete kaldi_feat_pipe_;
     if (mean_var_norm_) {
       delete mean_var_norm_;
     }
   }
 
   i32 FeatureDim() {
-    return kaldi_feature_pipeline_->Dim();
+    return kaldi_feat_pipe_->Dim();
   }
 
   void PushAudio(const float* samples, size_t num_samples, float sample_rate) {
-    kaldi_feature_pipeline_->AcceptWaveform(
+    kaldi_feat_pipe_->AcceptWaveform(
       sample_rate, 
       kaldi::SubVector<float>(samples, num_samples)
     );
   }
 
   void EndOfAudio() {
-    kaldi_feature_pipeline_->InputFinished();
+    kaldi_feat_pipe_->InputFinished();
   }
 
   i32 NumFrames() const {
-    return kaldi_feature_pipeline_->NumFramesReady();
+    return kaldi_feat_pipe_->NumFramesReady();
   }
 
   void GetFrame(i32 frame_idx, kaldi::VectorBase<float>* frame) {
-    kaldi_feature_pipeline_->GetFrame(frame_idx, frame);
+    kaldi_feat_pipe_->GetFrame(frame_idx, frame);
     if (mean_var_norm_) {
       mean_var_norm_->Normalize(frame);
     }
   }
 
   void Reset() {
-    delete kaldi_feature_pipeline_;
-    kaldi_feature_pipeline_ = new kaldi::OnlineNnet2FeaturePipeline(info_);
+    delete kaldi_feat_pipe_;
+    kaldi_feat_pipe_ = new kaldi::OnlineNnet2FeaturePipeline(kaldi_feat_info_);
   }
 
  private:
-  kaldi::OnlineNnet2FeaturePipelineInfo info_;
-  Owner<kaldi::OnlineNnet2FeaturePipeline*> kaldi_feature_pipeline_;
-  Optional<Owner<MeanVarNorm*>> mean_var_norm_ = nullptr;
+  kaldi::OnlineNnet2FeaturePipelineInfo kaldi_feat_info_;
+  Owner<kaldi::OnlineNnet2FeaturePipeline*> kaldi_feat_pipe_;
+  Optional<Owner<MeanVarNorm*>> mean_var_norm_;
 }; // class FeatureExtractor
 
 }  // namespace sio
