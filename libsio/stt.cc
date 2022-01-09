9dc49a01078bf45975a8cec047b54a956cd2e4c3
dophist <jerry.ejwt@qq.com>
2022-01-09 16:39:44 +0800

    add absl::SkipWhitespace() to all StrSplit() calls

--- libsio/stt.cc
+++ libsio/stt.cc
@@ -1,55 +1,55 @@
 #include <iostream>
 
 #include "stt.h"
+#include "sio/dbg.h"
 
 int main() {
-	sio::SpeechToText stt;
-	stt.Load("testdata/stt.json");
+    sio::SpeechToText stt;
+    stt.Load("testdata/stt.json");
 
-	int samples_per_chunk = std::numeric_limits<int>::max();
-	if (stt.config.online == true) {
-		samples_per_chunk = 1000;
-	}
+    int samples_per_chunk = std::numeric_limits<int>::max();
+    if (stt.config.online == true) {
+        samples_per_chunk = 1000;
+    }
 
-	sio::Recognizer* rec = stt.CreateRecognizer(); assert(rec != nullptr);
+    sio::Recognizer* rec = stt.CreateRecognizer(); assert(rec != nullptr);
 
-	//std::ifstream wav_scp("testdata/MINI/wav.scp");
-	std::ifstream wav_scp("testdata/aishell1-test.scp");
-	std::string line;
-	int k = 0;
-	while (std::getline(wav_scp, line)) {
-		std::vector<std::string> fields = absl::StrSplit(line, absl::ByAnyChar(" \t,:;"));
-		if (fields.size() != 2) continue;
+    //std::ifstream wav_scp("testdata/MINI/wav.scp");
+    std::ifstream wav_scp("testdata/aishell1-test.scp");
+    std::string line;
+    int k = 0;
+    while (std::getline(wav_scp, line)) {
+        std::vector<std::string> fields = absl::StrSplit(line, absl::ByAnyChar(" \t"), absl::SkipWhitespace());
+        if (fields.size() != 2) continue;
 
-		std::string& audio_id   = fields[0];
-		std::string& audio_path = fields[1];
-		SIO_DEBUG << audio_id << " " << audio_path;
+        std::string& audio_id   = fields[0];
+        std::string& audio_path = fields[1];
+        SIO_DEBUG << audio_id << " " << audio_path;
 
-		std::vector<float> audio;
-		float sample_rate;
-		sio::ReadAudio(audio_path, &audio, &sample_rate);
+        std::vector<float> audio;
+        float sample_rate;
+        sio::ReadAudio(audio_path, &audio, &sample_rate);
 
+        int N = 0;
+        while (N < audio.size()) {
+            int n = std::min(samples_per_chunk , (int)audio.size() - N);
+            rec->Speech(
+                audio.data() + N,
+                n,
+                sample_rate
+            );
+            N += n;
+        }
+        rec->To();
 
-		int N = 0;
-		while (N < audio.size()) {
-			int n = std::min(samples_per_chunk , (int)audio.size() - N);
-			rec->Speech(
-				audio.data() + N,
-				n,
-				sample_rate
-			);
-			N += n;
-		}
-		rec->To();
+        std::string text;
+        rec->Text(&text);
+        rec->Reset();
+        SIO_DEBUG << audio_id << " -> " << N << " samples decoded: " << text;
+        SIO_INFO << k++ << "\t" << audio_id << "\t" << text;
+    }
 
-		std::string text;
-		rec->Text(&text);
-		rec->Reset();
-		SIO_DEBUG << audio_id << " -> " << N << " samples decoded: " << text;
-		SIO_INFO << k++ << "\t" << audio_id << "\t" << text;
-	}
+    stt.DestroyRecognizer(rec);
 
-	stt.DestroyRecognizer(rec);
-
-	return 0;
+    return 0;
 }
