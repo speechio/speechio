5d8dc2f2d72bf5b22f562400cb8d6e23881c4a80
dophist <jerry.ejwt@qq.com>
2022-01-10 14:05:05 +0800

    clean base.h with SIO_UNDEFINED*

--- libsio/src/base.h
+++ libsio/src/base.h
@@ -11,7 +11,6 @@ namespace sio {
 #define SIO_LIKELY   ABSL_PREDICT_TRUE
 #define SIO_UNLIKELY ABSL_PREDICT_FALSE
 
-#define SIO__FUNC__ __func__
 /*
 #if defined(_MSC_VER)
 #define SIO_FUNC_REPR __FUNCSIG__
@@ -21,6 +20,7 @@ namespace sio {
 #define SIO_FUNC_REPR __func__
 #endif
 */
+#define SIO__FUNC__ __func__
 
 constexpr const char* Basename(const char* fname, int offset) {
     return offset == 0 || fname[offset - 1] == '/' || fname[offset - 1] == '\\'
@@ -30,13 +30,13 @@ constexpr const char* Basename(const char* fname, int offset) {
 #define SIO__FILE__  ::sio::Basename(__FILE__, sizeof(__FILE__) - 1)
 
 
-//#define SIO_UNDEF_BYTE 0xAA  /* Zig uses 0xAA */
-#define SIO_UNDEF_BYTE 0x00
+//#define SIO_UNDEFINED_BYTE 0xAA  /* Zig uses 0xAA */
+#define SIO_UNDEFINED_BYTE 0x00
 
 template<typename T>
 constexpr T undefined_value() {
     T t;
-    memset(&t, SIO_UNDEF_BYTE, sizeof(t));
+    memset(&t, SIO_UNDEFINED_BYTE, sizeof(t));
     return t;
 }
 
