b7f81e77f96dd25821b84e5858860c5b2f03e449
dophist <jerry.ejwt@qq.com>
2022-01-23 01:25:54 +0800

    finish fsm class draft and unittest

--- libsio/src/fsm.h
+++ libsio/src/fsm.h
@@ -1,12 +1,15 @@
 #ifndef SIO_FSM_H
 #define SIO_FSM_H
 
+#include <algorithm>
 #include "base/io-funcs.h"
 
 #include "sio/common.h"
+//#include "sio/dbg.h"
 
 namespace sio {
-struct Fsm {
+class Fsm {
+public:
     /********** Types **********/
     using StateId = i32;
     using ArcId   = i32;
@@ -31,6 +34,14 @@ struct Fsm {
         Weight weight = 0.0f;
 
         ArcAux aux;
+
+        void Load(StateId s, StateId d, LabelId i, Weight w, LabelId o) {
+            src = s;
+            dst = d;
+            label = i;
+            weight = w;
+            aux.label = o;
+        }
     };
 
 
@@ -50,41 +61,43 @@ struct Fsm {
     };
 
 
+private:
     /********** Data Members **********/
-    Str version; // TODO: make version a part of binary header
+    Str version_; // TODO: make version a part of binary header
 
-    StateId start_state = 0;
-    StateId final_state = 0;
+    StateId start_state_ = 0;
+    StateId final_state_ = 0;
 
-    Vec<State> states;
-    Vec<Arc> arcs;
+    Vec<State> states_;
+    Vec<Arc> arcs_;
 
 
+public:
     /********** Methods **********/
     inline bool Empty() const {
-        return states.size() == 0;
+        return states_.size() == 0;
     }
 
 
-    inline StateId Start() const { SIO_CHECK(!Empty()); return start_state; }
-    inline StateId Final() const { SIO_CHECK(!Empty()); return final_state; }
+    inline StateId Start() const { SIO_CHECK(!Empty()); return start_state_; }
+    inline StateId Final() const { SIO_CHECK(!Empty()); return final_state_; }
 
 
     /*
     inline const State& GetState(StateId i) const {
         SIO_CHECK(!Empty());
-        SIO_CHECK_NE(i, states.size() - 1); // block external access to sentinel
-        return states[i];
+        SIO_CHECK_NE(i, states_.size() - 1); // block external access to sentinel
+        return states_[i];
     }
     */
 
 
     ArcIterator GetArcIterator(StateId i) const {
         SIO_CHECK(!Empty());
-        SIO_CHECK_NE(i, states.size() - 1); // block external access to sentinel
+        SIO_CHECK_NE(i, states_.size() - 1); // block external access to sentinel
         return ArcIterator(
-            &arcs[states[i].arcs_begin],
-            &arcs[states[i+1].arcs_begin]
+            &arcs_[states_[i].arcs_begin],
+            &arcs_[states_[i+1].arcs_begin]
         );
     }
 
@@ -92,28 +105,31 @@ struct Fsm {
     /*
     inline size_t NumArcsOf(StateId i) const {
         SIO_CHECK(!Empty());
-        return states[i+1].arcs_begin - states[i].arcs_begin;
+        return states_[i+1].arcs_begin - states_[i].arcs_begin;
     }
     */
 
 
     u64 NumStates() const {
         SIO_CHECK(!Empty());
-        return states.size() - 1; // -1: last sentinel is invalid Fsm state
+        return states_.size() - 1; // -1: last sentinel is invalid Fsm state
     }
 
 
     u64 NumArcs() const {
         SIO_CHECK(!Empty());
-        return arcs.size(); 
+        return arcs_.size(); 
     }
 
 
-    Error Load(std::istream &is, bool binary) {
-        SIO_CHECK(Empty()) << "Fsm already loaded ?";
+    Error Load(std::istream& is) {
+        SIO_CHECK(Empty()) << "Reloading is not supported.";
+        SIO_CHECK(is.good());
 
         using kaldi::ExpectToken;
         using kaldi::ReadBasicType;
+
+        bool binary = true;
 
         ExpectToken(is, binary, "<Fsm>");
 
@@ -121,39 +137,45 @@ struct Fsm {
         TODO: version handling here
         */
 
-        ExpectToken(is, binary, "<Start>");
-        ReadBasicType(is, binary, &start_state);
-        SIO_CHECK(start_state == 0); // conform to K2
-
-        ExpectToken(is, binary, "<Final>");
-        ReadBasicType(is, binary, &final_state);
-
         ExpectToken(is, binary, "<NumStates>");
         u64 num_states = 0;
         ReadBasicType(is, binary, &num_states);
-        SIO_CHECK_EQ(final_state, num_states - 1); // conform to K2
-
-        ExpectToken(is, binary, "<States>");
-        auto num_states_plus_sentinel = num_states + 1;
-        states.resize(num_states_plus_sentinel);
-        is.read(reinterpret_cast<char*>(states.data()), num_states_plus_sentinel * sizeof(State));
 
         ExpectToken(is, binary, "<NumArcs>");
         u64 num_arcs = 0;
         ReadBasicType(is, binary, &num_arcs);
 
+        ExpectToken(is, binary, "<Start>");
+        ReadBasicType(is, binary, &start_state_);
+        SIO_CHECK(start_state_ == 0); // conform to K2
+
+        ExpectToken(is, binary, "<Final>");
+        ReadBasicType(is, binary, &final_state_);
+        SIO_CHECK_EQ(final_state_, num_states - 1); // conform to K2
+
+        ExpectToken(is, binary, "<States>");
+        auto num_states_plus_sentinel = num_states + 1;
+        states_.resize(num_states_plus_sentinel);
+        is.read(reinterpret_cast<char*>(states_.data()), num_states_plus_sentinel * sizeof(State));
+
         ExpectToken(is, binary, "<Arcs>");
-        arcs.resize(num_arcs);
-        is.read(reinterpret_cast<char*>(arcs.data()), num_arcs * sizeof(Arc));
+        arcs_.resize(num_arcs);
+        is.read(reinterpret_cast<char*>(arcs_.data()), num_arcs * sizeof(Arc));
+
+        //DebugPrint();
 
         return Error::OK;
     }
 
 
-    Error Dump(std::ostream &os, bool binary) const {
+    Error Dump(std::ostream& os) const {
         SIO_CHECK(!Empty()) << "Dumping empty Fsm ?";
+        SIO_CHECK(os.good());
+
         using kaldi::WriteToken;
         using kaldi::WriteBasicType;
+
+        bool binary = true;
 
         WriteToken(os, binary, "<Fsm>");
 
@@ -161,39 +183,112 @@ struct Fsm {
         TODO: version handling here
         */
 
-        WriteToken(os, binary, "<Start>");
-        WriteBasicType(os, binary, Start());
-
-        WriteToken(os, binary, "<Final>");
-        WriteBasicType(os, binary, Final());
-
         WriteToken(os, binary, "<NumStates>");
         WriteBasicType(os, binary, NumStates());
 
         WriteToken(os, binary, "<NumArcs>");
         WriteBasicType(os, binary, NumArcs());
 
+        WriteToken(os, binary, "<Start>");
+        WriteBasicType(os, binary, Start());
+
+        WriteToken(os, binary, "<Final>");
+        WriteBasicType(os, binary, Final());
+
         WriteToken(os, binary, "<States>");
         auto num_states_plus_sentinel = NumStates() + 1;
-        os.write(reinterpret_cast<const char*>(states.data()), num_states_plus_sentinel * sizeof(State));
+        os.write(reinterpret_cast<const char*>(states_.data()), num_states_plus_sentinel * sizeof(State));
 
         WriteToken(os, binary, "<Arcs>");
-        os.write(reinterpret_cast<const char*>(arcs.data()), NumArcs() * sizeof(Arc));
+        os.write(reinterpret_cast<const char*>(arcs_.data()), NumArcs() * sizeof(Arc));
 
         return Error::OK;
     }
 
 
-    // TODO
-    Error LoadOpenFst(std::istream& is) {
+    Error LoadFromString(std::istream& is) {
+        SIO_CHECK(is.good()) << "Invalid Fsm loading stream.";
+        SIO_CHECK(Empty()) << "Reloading is not supported.";
+
         Str line;
+        Vec<Str> cols;
+
+        // header line: num_states num_arcs start_state final_state
+        std::getline(is, line);
+        cols = absl::StrSplit(line, ',', absl::SkipWhitespace());
+        SIO_CHECK_EQ(cols.size(), 4);
+        size_t num_states = std::stoi(cols[0]);
+        size_t num_arcs   = std::stoi(cols[1]);
+        start_state_ = std::stoi(cols[2]);
+        final_state_ = std::stoi(cols[3]);
+        //dbg(num_states, num_arcs, start_state_, final_state_);
+
+        // allocate space for states and arcs
+        states_.resize(num_states + 1); // plus 1 sentinel
+        arcs_.resize(num_arcs);
+
+        // load arcs
+        Vec<i32> num_arcs_of_state(num_states, 0);
+        ArcId a = 0;
         while (std::getline(is, line)) {
-            Vec<Str> cols = absl::StrSplit(line, absl::ByAnyChar(" \t"), absl::SkipWhitespace());
+            cols = absl::StrSplit(line, absl::ByAnyChar(" \t"), absl::SkipWhitespace());
+            //dbg(cols);
+            SIO_CHECK(cols.size() == 4);
+            Vec<Str> labels = absl::StrSplit(cols[2], ':');
+            SIO_CHECK(labels.size() == 1 || labels.size() == 2); // 1:Fsa,  2:Fst
+
+            StateId src = std::stoi(cols[0]);
+            StateId dst = std::stoi(cols[1]);
+            LabelId label = std::stoi(labels[0]);
+            LabelId aux_label = labels.size() == 2 ? std::stoi(labels[1]) : 0;
+            Weight weight = std::stof(cols[3]);
+
+            arcs_[a].src = src;
+            arcs_[a].dst = dst;
+            arcs_[a].label = label;
+            arcs_[a].weight = weight;
+            arcs_[a].aux.label = aux_label;
+
+            ++num_arcs_of_state[src];
+            a++;
         }
 
+        // sort labels first by src state, then by dst state
+        std::sort(arcs_.begin(), arcs_.end(), [](const Arc& x, const Arc& y) { 
+            if (x.src != y.src) {
+                return x.src < y.src; 
+            } else {
+                return x.dst < y.dst; 
+            }
+        });
+
+        // load states, invariant: states_[s].arcs_begin = sum of arcs of states_[0, s)
+        StateId s = 0;
+        size_t n = 0;
+        while (s < num_states) {
+            states_[s].arcs_begin = n;
+            n += num_arcs_of_state[s++];
+        }
+        states_[s].arcs_begin = n; // setup sentinel state
+
+        //DebugPrint();
+
         return Error::OK;
     }
 
-}; // struct Fsm
+
+    /*
+    void DebugPrint() {
+        for (const auto& s : states_) {
+            dbg(s.arcs_begin);
+        }
+
+        for (const auto& a : arcs_) {
+            dbg(a.src, a.dst, a.label, a.weight, a.aux.label);
+        }
+    }
+    */
+
+}; // class Fsm
 } // namespace sio
 #endif
--- libsio/src/fsm_test.cc
+++ libsio/src/fsm_test.cc
@@ -1,10 +1,38 @@
 #include <gtest/gtest.h>
+
+#include <fstream>
 #include "sio/fsm.h"
 
 //#include "sio/dbg.h"
 
 namespace sio {
 TEST(Fsm, Basic) {
-    Fsm fsa;
+    Fsm fsm;
+    std::ifstream is("testdata/graph.int");
+    fsm.LoadFromString(is);
+
+    {
+        std::ofstream os("testdata/graph.fsm", std::ios::binary);
+        fsm.Dump(os);
+    }
+
+    Fsm fsm2;
+    {
+        std::ifstream is2("testdata/graph.fsm", std::ios::binary);
+        fsm2.Load(is2);
+    }
+
+    EXPECT_EQ(fsm2.NumStates(), 4);
+    EXPECT_EQ(fsm2.NumArcs(), 8);
+    EXPECT_EQ(fsm2.Start(), 0);
+    EXPECT_EQ(fsm2.Final(), 3);
+
+    printf("%d,%d,%d,%d\n", fsm2.NumStates(), fsm2.NumArcs(), fsm2.Start(), fsm2.Final());
+    for (Fsm::StateId s = 0; s < fsm2.NumStates(); s++) {
+        for (auto ai = fsm2.GetArcIterator(s); !ai.Done(); ai.Next()) {
+            const Fsm::Arc& arc = ai.Value();
+            printf("%d\t%d\t%d:%d\t%f\n", arc.src, arc.dst, arc.label, arc.aux.label, arc.weight);
+        }
+    }
 }
 }
