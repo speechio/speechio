51e89c4c3a78f115a54c8a22d9653f23a4b20d54
dophist <jerry.ejwt@qq.com>
2022-02-08 01:03:02 +0800

    fix scorer with true streaming processing

--- libsio/sio/scorer.h
+++ libsio/sio/scorer.h
@@ -24,8 +24,7 @@ struct ScorerConfig {
 };
 
 
-struct Scorer {
-private:
+class Scorer {
     ScorerConfig config_;
     torch::jit::script::Module* nnet_ = nullptr;
     int nnet_idim_ = 0;
@@ -79,64 +78,15 @@ public:
     void Push(const Vec<f32>& feat_frame) {
         feat_cache_.emplace_back(feat_frame);
         ++cur_feat_frame_;
+
+        if (feat_cache_.size() == config_.chunk_size * subsampling_factor_ + right_context_) {
+            Advance();
+        }
     }
 
 
     void PushEnd() {
-        torch::NoGradGuard no_grad;
-
-        // init empty feature tensor
-        torch::Tensor feats = torch::zeros(
-            {
-                1, 
-                static_cast<long>(feat_cache_.size()), 
-                nnet_idim_
-            }, 
-            torch::kFloat
-        );
-
-        // fill feature tensor with cached feature frames
-        for (index_t f = 0; f != feat_cache_.size(); f++) {
-            torch::Tensor frame_tensor = torch::from_blob(
-                feat_cache_[f].data(),
-                { nnet_idim_ },
-                torch::kFloat
-            ).clone();
-            feats[0][f] = std::move(frame_tensor);
-        }
-
-        // FIX THIS: extremely confusing units due to subsampling factor
-        // here offset refers to sub-sampled frames
-        // assemble feature and caches as input
-        int requried_cache_size = config_.chunk_size * config_.num_left_chunks;
-        Vec<torch::jit::IValue> inputs = {
-            feats,
-            cur_score_frame_,
-            requried_cache_size,
-            subsampling_cache_,
-            elayers_output_cache_,
-            conformer_cnn_cache_
-        };
-
-        // Forward encoder layers
-        auto r = nnet_->get_method("forward_encoder_chunk")(inputs).toTuple()->elements();
-
-        SIO_CHECK_EQ(r.size(), 4);
-        torch::Tensor acoustic_encoding = r[0].toTensor();
-        // what is "assign" semantics for torch::jit:IValue ?
-        subsampling_cache_ = r[1];
-        elayers_output_cache_ = r[2];
-        conformer_cnn_cache_ = r[3];
-        acoustic_encoding_cache_.push_back(acoustic_encoding);
-
-        // scores: [frames, nnet_odim]
-        torch::Tensor scores = nnet_->run_method("ctc_activation", acoustic_encoding).toTensor()[0];
-
-        for (index_t s = 0; s != scores.size(0); s++) {
-            scores_cache_.push_back(scores[s]);
-            ++cur_score_frame_;
-        }
-        //dbg(scores_cache_.size(0), scores_cache_.size(1));
+        Advance();
     }
 
 
@@ -169,6 +119,70 @@ public:
 
     size_t Dim() const {
         return 0; // TODO: this should be the dim of nnet output
+    }
+
+private:
+    Error Advance() {
+        if (feat_cache_.size() <= right_context_) {
+            return Error::OK;
+        }
+        //dbg(cur_feat_frame_, feat_cache_.size());
+
+        torch::NoGradGuard no_grad;
+
+        // Prepare feature chunk tensor
+        // with shape [batch_size, num_cached_frames, feature_dim]
+        // batch_size will always be 1
+        torch::Tensor chunk_feat = torch::zeros(
+            {1, static_cast<long>(feat_cache_.size()), nnet_idim_}, 
+            torch::kFloat
+        );
+        for (index_t f = 0; f != feat_cache_.size(); f++) {
+            torch::Tensor frame_tensor = torch::from_blob(
+                feat_cache_[f].data(),
+                { nnet_idim_ },
+                torch::kFloat
+            ).clone();
+            chunk_feat[0][f] = std::move(frame_tensor);
+        }
+        while (feat_cache_.size() > right_context_) {
+            feat_cache_.pop_front();
+        }
+
+        // FIX THIS: extremely confusing units due to subsampling factor
+        // here offset refers to sub-sampled frames
+        // assemble feature and caches as input
+        int requried_cache_size = config_.chunk_size * config_.num_left_chunks;
+        Vec<torch::jit::IValue> chunk_input = {
+            chunk_feat,
+            cur_score_frame_,
+            requried_cache_size,
+            subsampling_cache_,
+            elayers_output_cache_,
+            conformer_cnn_cache_
+        };
+
+        // Forward encoder layers
+        auto r = nnet_->get_method("forward_encoder_chunk")(chunk_input).toTuple()->elements();
+
+        SIO_CHECK_EQ(r.size(), 4);
+        torch::Tensor acoustic_encoding = r[0].toTensor();
+        // what is "assign" semantics for torch::jit:IValue ?
+        subsampling_cache_ = r[1];
+        elayers_output_cache_ = r[2];
+        conformer_cnn_cache_ = r[3];
+        acoustic_encoding_cache_.push_back(acoustic_encoding);
+
+        // scores: [frames, nnet_odim]
+        torch::Tensor scores = nnet_->run_method("ctc_activation", acoustic_encoding).toTensor()[0];
+
+        for (index_t s = 0; s != scores.size(0); s++) {
+            scores_cache_.push_back(scores[s]);
+            ++cur_score_frame_;
+        }
+        //dbg(scores_cache_.size(0), scores_cache_.size(1));
+  
+        return Error::OK;
     }
 
 }; // class scorer.h
