6259de4b96410280ab311ce95495efafb1136c0c
dophist <jerry.ejwt@qq.com>
2022-02-08 23:47:06 +0800

    refine format

--- recipe.sh
+++ recipe.sh
@@ -18,36 +18,30 @@ if [ $stage -le 0 ]; then
 fi
 
 if [ $stage -le 1 ]; then
-    echo "Training tokenizer from text ..."
-    ops/tokenizer_train --config $dir/tokenizer.yaml \
-        --input $text \
-        --model $dir/tokenizer \
-        2> $dir/log.tokenizer
+    echo "Training tokenizer from text: "
+    ops/tokenizer_train --config $dir/tokenizer.yaml --input $text --model $dir/tokenizer 2>$dir/log.tokenizer
 fi
 
 if [ $stage -le 2 ]; then
     echo "Traning stt model ..."
-    ops/stt_train --node_rank 0 --config $dir/train.yaml $dir 2> $dir/log.train
+    ops/stt_train --node_rank 0 --config $dir/train.yaml $dir 2>$dir/log.train
 fi
 
 if [ $stage -le 3 ]; then
     echo "Averaging model checkpoints ..."
-    ops/stt_average $dir/checkpoints $dir/final.model 2> $dir/log.average
+    ops/stt_average $dir/checkpoints $dir/final.model 2>$dir/log.average
 
     echo "Exporting torchscript model ..."
-    ops/stt_export --config $dir/train.yaml $dir/final.model $dir/final.ts 2> $dir/log.export
+    ops/stt_export --config $dir/train.yaml $dir/final.model $dir/final.ts 2>$dir/log.export
 fi
 
 if [ $stage -le 4 ]; then
     echo "Decoding test set ..."
-    ops/stt --config $dir/test.yaml $dir 1> $dir/test.txt 2> $dir/log.test
+    ops/stt --config $dir/test.yaml $dir 1> $dir/test.txt 2>$dir/log.test
 fi
 
 if [ $stage -le 5 ]; then
     echo "Evaluating error rate ..."
-    awk -F'\t' '{print $2, $3}' $dir/test.txt > $dir/rec.txt
-    ops/stt_error_rate --tokenizer char \
-        --ref $dir/ref.txt \
-        --hyp $dir/rec.txt \
-        $dir/RESULT 2> $dir/log.eval
+    awk -F'\t' '{print $2, $3}' $dir/test.txt >$dir/rec.txt
+    ops/stt_error_rate --tokenizer char --ref $dir/ref.txt --hyp $dir/rec.txt $dir/RESULT 2>$dir/log.eval
 fi
