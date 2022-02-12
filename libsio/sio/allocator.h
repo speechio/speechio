c22619576fe0627eb40de8f89903da3ca02beef8
dophist <jerry.ejwt@qq.com>
2022-02-12 16:55:52 +0800

    add length field to FreeList

--- libsio/sio/allocator.h
+++ libsio/sio/allocator.h
@@ -4,7 +4,6 @@
 #include "sio/ptr.h"
 #include "sio/check.h"
 #include "sio/vec.h"
-#include "sio/dbg.h"
 
 namespace sio {
 
@@ -15,6 +14,7 @@ struct FreeNode {
 
 struct FreeList {
     Optional<FreeNode*> head = nullptr;
+    size_t length = 0;
 
     inline bool IsEmpty() {
         return (head == nullptr);
@@ -23,12 +23,14 @@ struct FreeList {
     inline void Push(FreeNode* p) {
         p->next = head;
         head = p;
+        length++;
     }
 
     inline FreeNode* Pop() {
         SIO_CHECK(!IsEmpty()); // caches should grow outside Pop(), in Alloc()
         FreeNode* p = head;
         head = head->next;
+        length--;
         return p;
     }
 };
@@ -48,7 +50,8 @@ public:
     void SetCacheSize(size_t size0, size_t size1 = 1) {
         SIO_CHECK(caches_.empty()) << "Cache size need to be set before uses.";
         SIO_CHECK_GE(size0, 1);
-        SIO_CHECK_GE(sizeof(T) * size1, sizeof(FreeNode*)) << "Size of allocator item should be larger than a pointer";
+        SIO_CHECK_GE(size1 * sizeof(T), sizeof(FreeNode*)) \
+            << "Cannot support allocations smaller than a pointer.";
 
         size0_ = size0;
         size1_ = size1;
@@ -57,17 +60,16 @@ public:
 
     inline T* Alloc() {
         if (free_list_.IsEmpty()) {
-            // add new cache
             caches_.resize(caches_.size() + 1);
             Vec<char>& c = caches_.back();
             c.resize(size0_ * size1_ * sizeof(T));
 
-            // link new cache elems with free list
             char* p = c.data();
             for (int i = 0; i < size0_; i++) {
                 free_list_.Push( (FreeNode*)(p + i * size1_ * sizeof(T)) );
             }
         }
+
         num_used_++;
         return (T*)free_list_.Pop();
     }
@@ -79,24 +81,18 @@ public:
     }
 
 
-    size_t NumUsed() const {
-        return num_used_;
-    }
-
-
-    size_t NumFree() const {
-        size_t n = 0;
-        for (Optional<FreeNode*> p = free_list_.head; p != nullptr; p = p->next) {
-            n++;
-        }
-        return n;
-    }
+    size_t NumUsed() const { return num_used_; }
+    size_t NumFree() const { return free_list_.length; }
 
 
     void clear() {
-        caches_.clear();
-        free_list_.head = nullptr;
         num_used_ = 0;
+
+        free_list_.head = nullptr;
+        free_list_.length = 0;
+
+        caches_.clear();
+
     }
 
 }; // class SlabAllocator
