804be9b0cbecdd624708bd0c6b439ae07a300513
dophist <jerry.ejwt@qq.com>
2022-02-21 22:08:38 +0800

    simplify slab allocator

--- libsio/sio/allocator.h
+++ libsio/sio/allocator.h
@@ -11,40 +11,17 @@ struct FreeNode {
     Nullable<FreeNode*> next = nullptr;
 };
 
-
-struct FreeList {
-    Nullable<FreeNode*> head = nullptr;
-    size_t length = 0;
-
-    inline bool Empty() {
-        return (head == nullptr);
-    }
-
-    inline void Push(FreeNode* p) {
-        p->next = head;
-        head = p;
-        length++;
-    }
-
-    inline FreeNode* Pop() {
-        SIO_CHECK(!Empty()); // slab should grow outside Pop(), in Alloc()
-        FreeNode* p = head;
-        head = head->next;
-        length--;
-        return p;
-    }
-};
-
-
 template <typename T>
 class SlabAllocator {
-    // One slab is a blob of raw memory with size0_ * size1_ * sizeof(T) bytes.
+    // A slab is a blob of raw memory with size0_ * size1_ * sizeof(T) bytes.
     size_t size0_ = 4096; // num of allocations of one slab
     size_t size1_ = 1;    // num of elems(of type T) of one allocation
-    Vec<Vec<char>> slabs_;
 
-    FreeList free_list_;
+    Vec<Vec<char>> slabs_;
+    Nullable<FreeNode*> free_list_ = nullptr;
+
     size_t num_used_ = 0;
+    size_t num_free_ = 0;
 
 public:
 
@@ -60,39 +37,54 @@ public:
 
 
     inline T* Alloc() {
-        if (free_list_.Empty()) {
+        if (free_list_ == nullptr) {
             slabs_.resize(slabs_.size() + 1);
-            Vec<char>& slab = slabs_.back();
-            slab.resize(size0_ * size1_ * sizeof(T));
+            Vec<char>& s = slabs_.back();
+            s.resize(size0_ * size1_ * sizeof(T));
 
-            char* p = slab.data();
             for (int i = 0; i < size0_; i++) {
-                free_list_.Push( (FreeNode*)(p + i * size1_ * sizeof(T)) );
+                FreeListPush( (FreeNode*)(s.data() + i * size1_ * sizeof(T)) );
             }
         }
 
         num_used_++;
-        return (T*)free_list_.Pop();
+        return (T*) FreeListPop();
     }
 
 
     inline void Free(T* p) {
         num_used_--;
-        free_list_.Push( (FreeNode*)p );
+        FreeListPush( (FreeNode*)p );
     }
 
 
     size_t NumUsed() const { return num_used_; }
-    size_t NumFree() const { return free_list_.length; }
+    size_t NumFree() const { return num_free_; }
 
 
     void Reset() {
-        num_used_ = 0;
-
-        free_list_.head = nullptr;
-        free_list_.length = 0;
-
         slabs_.clear();
+        free_list_ = nullptr;
+
+        num_used_ = 0;
+        num_free_ = 0;
+    }
+
+private:
+
+    inline void FreeListPush(FreeNode* p) {
+        p->next = free_list_;
+        free_list_ = p;
+        ++num_free_;
+    }
+
+
+    inline FreeNode* FreeListPop() {
+        SIO_CHECK(free_list_ != nullptr); // slab should grow outside Pop(), in Alloc()
+        FreeNode* p = free_list_;
+        free_list_ = free_list_->next;
+        --num_free_;
+        return p;
     }
 
 }; // class SlabAllocator
