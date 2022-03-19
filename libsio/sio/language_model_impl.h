e189248c6f4aae2af0edce9a64297be7150066f8
dophist <jerry.ejwt@qq.com>
2022-03-19 22:40:48 +0800

    simplify PrefixTreeLM to stateless implimentation

--- libsio/sio/language_model_impl.h
+++ libsio/sio/language_model_impl.h
@@ -3,54 +3,24 @@
 
 namespace sio {
 
-class PrefixLm : public LanguageModel {
-    // prefix word sequence is represented with a unique hash of type u64
+class PrefixTreeLM : public LanguageModel {
+    // prefix word sequence is represented with a unique hash of type u32
     using PrefixUID = u32;
-
-    Vec<PrefixUID> state_to_prefix_;
-    Map<PrefixUID, LmStateId> prefix_to_state_;
-
 public:
 
-    Error Load() {
-        // Establish null lm state: state index = 0, prefix hash = 0
-        SIO_CHECK(state_to_prefix_.empty() && prefix_to_state_.empty());
-        state_to_prefix_.push_back(0);
-        prefix_to_state_[0] = 0;
-
-        //dbg(prefix_to_state_);
-        //dbg(state_to_prefix_);
-
-        return Error::OK;
-    }
-
-
     LmStateId NullState() const override {
-        SIO_CHECK(!prefix_to_state_.empty()) << "PrefixLm uninitialzed ?";
         return 0;
     }
-
 
     bool GetScore(LmStateId src_state, LmWordId word, LmScore* score, LmStateId* dst_state) override {
         // prime are picked from Kaldi's VectorHasher:
         //   https://github.com/kaldi-asr/kaldi/blob/master/src/util/stl-utils.h#L230
         constexpr u32 prime = 7853;
-
-        *score = 0.0;
-
-        PrefixUID src = state_to_prefix_[src_state];
+        PrefixUID src = (PrefixUID)src_state;
         PrefixUID dst = src * prime + (u32)word; // incremental sequence hashing
 
-        auto it = prefix_to_state_.find(dst);
-        if (it == prefix_to_state_.end()) {
-            LmStateId s = state_to_prefix_.size();
-            state_to_prefix_.push_back(dst);
-            prefix_to_state_[dst] = s;
-
-            *dst_state = s;
-        } else {
-            *dst_state = it->second;
-        }
+        *score = 0.0;
+        *dst_state = (LmStateId)dst;
 
         return true;
     }
--- libsio/sio/language_model_test.cc
+++ libsio/sio/language_model_test.cc
@@ -11,36 +11,28 @@ TEST(LanguageModel, PrefixLm) {
     tokenizer.Load("testdata/tokenizer.vocab");
     //dbg(tokenizer.index_to_token);
 
-    PrefixLm prefix_lm;
-    prefix_lm.Load();
+    PrefixTreeLM prefix_lm;
+    LanguageModel* lm = &prefix_lm;
 
-    LanguageModel* m = &prefix_lm;
-
-    LmStateId null_state = m->NullState();
+    LmStateId null_state = lm->NullState();
     EXPECT_EQ(null_state, 0); // null state is index as 0
 
     LmScore s;
 
     LmStateId bos_state;
-    m->GetScore(null_state, tokenizer.Index("<s>"), &s, &bos_state);
-    EXPECT_EQ(bos_state, 1);
+    lm->GetScore(null_state, tokenizer.Index("<s>"), &s, &bos_state);
 
     LmStateId a;
-    m->GetScore(bos_state, tokenizer.Index("a"), &s, &a);
-    EXPECT_EQ(a, 2);
+    lm->GetScore(bos_state, tokenizer.Index("a"), &s, &a);
 
     LmStateId aa;
-    m->GetScore(a, tokenizer.Index("a"), &s, &aa);
-    EXPECT_EQ(aa, 3);
+    lm->GetScore(a, tokenizer.Index("a"), &s, &aa);
 
     LmStateId ab;
-    m->GetScore(a, tokenizer.Index("b"), &s, &ab);
-    EXPECT_EQ(ab, 4);
+    lm->GetScore(a, tokenizer.Index("b"), &s, &ab);
 
     LmStateId ab_eos;
-    m->GetScore(ab, tokenizer.Index("</s>"), &s, &ab_eos);
-    EXPECT_EQ(ab_eos, 5);
-
+    lm->GetScore(ab, tokenizer.Index("</s>"), &s, &ab_eos);
 }
 
 } // namespace sio
