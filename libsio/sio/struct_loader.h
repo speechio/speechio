e574b9335b3891e9c4a65fd1c96d548da4d2c047
dophist <jerry.ejwt@qq.com>
2022-03-22 19:49:18 +0800

    fix includes order in struct loader

--- libsio/sio/struct_loader.h
+++ libsio/sio/struct_loader.h
@@ -6,9 +6,9 @@
 
 #include "sio/base.h"
 #include "sio/error.h"
+#include "sio/logger.h"
 #include "sio/check.h"
 #include "sio/ptr.h"
-#include "sio/logger.h"
 #include "sio/type.h"
 #include "sio/vec.h"
 #include "sio/str.h"
