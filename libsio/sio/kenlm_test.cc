b581dfc91b259fd31f20a4b991e95031f3c7d6f9
dophist <jerry.ejwt@qq.com>
2022-03-30 16:51:20 +0800

    cosmetic

--- libsio/sio/kenlm_test.cc
+++ libsio/sio/kenlm_test.cc
@@ -12,22 +12,22 @@ TEST(KenLM, Basic) {
     KenLM lm;
     lm.Load("testdata/model/lm.trie", tokenizer);
 
-    std::ifstream is("testdata/sentences.txt");
+    std::ifstream test_cases("testdata/sentences.txt");
 
     Str sentence;
-    while(std::getline(is, sentence)) {
+    while(std::getline(test_cases, sentence)) {
         Vec<Str> words = absl::StrSplit(sentence, " ");
         Str log = "[KenLM]";
         
         KenLM::State state[2];
-        KenLM::State* istate = &state[0];
-        KenLM::State* ostate = &state[1];
+        KenLM::State* is = &state[0];
+        KenLM::State* os = &state[1];
 
-        lm.SetStateToNull(istate);
+        lm.SetStateToNull(is);
         for (const auto& w : words) {
-            f32 score = lm.Score(istate, lm.GetWordIndex(w), ostate);
+            f32 score = lm.Score(is, lm.GetWordIndex(w), os);
             log += " " + w + "[" + std::to_string(lm.GetWordIndex(w)) + "]=" + std::to_string(score);
-            std::swap(istate, ostate);
+            std::swap(is, os);
         }
         SIO_INFO << log;
     }
