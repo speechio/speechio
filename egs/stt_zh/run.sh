27b86d04abc5684c90186a63b20e6711d9760146
dophist <jerry.ejwt@qq.com>
2022-04-10 22:21:44 +0800

    merge parallel tokenizer into tokenizer_encode

--- egs/stt_zh/run.sh
+++ egs/stt_zh/run.sh
@@ -56,9 +56,7 @@ fi
 
 if [ $stage -le 7 ]; then
     echo "Apply trained tokenizer to raw text ..."
-    #ops/tokenizer_encode  --model tokenizer.model  --input text.txt  --output lm.txt
-    ops/text_tokenize  $nj  tokenizer.model  text.txt  ./text_tokenize_dir/text
-    rm -f lm.txt && ln -s ./text_tokenize_dir/text.txt lm.txt
+    ops/tokenizer_encode  --nj $nj  --model tokenizer.model  --input text.txt  --output lm.txt
 
     echo "Training ARPA from tokenized text ..."
     ops/lm_train  --config lm.yaml  --text lm.txt  --vocab tokenizer.vocab  --model lm
