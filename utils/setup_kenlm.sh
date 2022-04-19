e08d5606a3a3912e8cbb2170ee0c0bdd6da84357
dophist <jerry.ejwt@qq.com>
2022-04-19 13:51:05 +0800

    add utils/setup_kenlm.sh

---
+++ utils/setup_kenlm.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+    echo "setup_kenlm.sh dir"
+    exit 1;
+fi
+
+dir=$1
+
+git clone --depth 1 https://github.com/kpu/kenlm $dir
+
+mkdir -p $dir/build && cd $dir/build
+cmake ..
+make -j 4
+cd -
+
+echo "KenLM has been installed to $dir/build/bin, please add it to your PATH"
+
