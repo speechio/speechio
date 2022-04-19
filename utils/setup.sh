bbf1aafc2b1fa8825c5b3276959221b511f950f6
dophist <jerry.ejwt@qq.com>
2022-04-19 13:53:05 +0800

    update setup.sh with kenlm

--- utils/setup.sh
+++ utils/setup.sh
@@ -21,6 +21,9 @@ pip3 install typeguard
 ## setup wenet python dependency
 utils/setup_wenet.sh
 
+## KenLM
+utils/setup_kenlm.sh utils/kenlm
+
 ## K2, problematic
 #conda install -c k2-fsa -c pytorch -c conda-forge k2 cudatoolkit=$CUDA_VERSION python=$PYTHON_VERSION pytorch=$PYTORCH_VERSION
 
