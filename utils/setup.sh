4a0a979ad200f6bc3e7cc15895a8bba834dcdb83
dophist <jerry.ejwt@qq.com>
2022-04-22 21:54:16 +0800

    add utils/setup_once.sh

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-#utils/setup_aliyun_oss.sh
-#utils/setup_cuda.sh
-#utils/setup_pyenv.sh
 utils/setup_wenet.sh
-#utils/setup_kenlm.sh ~/work/git/kenlm
 
---
+++ utils/setup_once.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+utils/setup_cuda.sh
+
+utils/setup_pyenv.sh
+
+utils/setup_kenlm.sh ~/work/git/kenlm
+
+utils/setup_aliyun_oss.sh
+
