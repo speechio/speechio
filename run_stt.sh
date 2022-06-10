8472ccbc5130df35b0ba0d5b3d3a1c66e4767862
dophist <jerry.ejwt@qq.com>
2022-06-10 10:09:13 +0800

    further simplify ops with short options

--- run_stt.sh
+++ run_stt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-raw_text=data/text/AISHELL-1_train.txt
+text_data=data/text/AISHELL-1_train.txt
 
 stage=0
 nj=40
@@ -16,7 +16,7 @@ fi
 
 if [ $stage -le 1 ]; then
     echo "normalizing raw text ..."
-    ops/text_norm -n $nj -i $raw_text -o text.txt 2>log.text_norm
+    ops/text_norm -n $nj -i ${text_data} -o text.txt 2>log.text_norm
 fi
 
 
@@ -28,29 +28,29 @@ fi
 
 if [ $stage -le 3 ]; then
     echo "Training stt model ..."
-    ops/stt_train  --config stt.yaml  --node_rank 0  .  2>log.train
+    ops/stt_train --node_rank 0 -c stt.yaml -o $(pwd) 2>log.train
 fi
 
 
 if [ $stage -le 4 ]; then
     echo "Averaging model checkpoints ..."
-    ops/stt_average  checkpoints  average.pt  2>log.average
+    ops/stt_average -n 20 -i checkpoints -o average.pt 2>log.average
 
     echo "Exporting torchscript model ..."
-    ops/stt_export  --config stt.yaml  average.pt  final.pts  2>log.export
+    ops/stt_export -c stt.yaml -i average.pt -o final.pts 2>log.export
 fi
 
 
 if [ $stage -le 5 ]; then
     echo "Decoding test set ..."
-    ops/stt_test  --config stt_test.yaml  .  1>test.tsv  2>log.test
+    ops/stt_test -c stt_test.yaml -o $(pwd) 1>test.tsv 2>log.test
 fi
 
 
 if [ $stage -le 6 ]; then
     echo "Evaluating error rate ..."
     awk -F'\t' '{print $2"\t"$3}' test.tsv >test.hyp
-    ops/stt_error_rate  --tokenizer char  --ref test.ref  --hyp test.hyp  RESULT  2>log.eval
+    ops/stt_error_rate  --token char  --ref test.ref  --hyp test.hyp  RESULT  2>log.eval
 fi
 
 
